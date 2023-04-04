c952b42bb600de96e38c742a6cc42ac3336e2da2
nikita shchenikov <nikita-shenikov@mail.ru>
2023-04-05 01:14:05 +0300

    hw5

--- script.js
+++ script.js
@@ -1,51 +1,153 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+// "use strict";
 
-//1 task
-// for(let i = 0; i <= 1; i++) {
-//     console.log('Привет');
-// }
+// window.addEventListener("DOMContentLoaded", (event) => {
 
-//2 task
-// let i = 1;
-// while(i <= 5) {
-//     console.log(i);
-//     i++;
-// }
-
-//3 task
-// let i = 7;
-// do {
-//     console.log(i);
-//     i++;
-// } while (i <= 22);
-
-//4 task
-// let obj = {
-//     Kolya: 200,
-//     Vasya: 300,
-//     Petya: 400
+//task 1
+// const min = (a, b) => {
+//     if(a > b) {
+//         return b;
+//     } else if (a < b) {
+//         return a;
+//     } else {
+//         return a;
+//     }
 // };
-// for (let property in obj) {
-//     console.log(`${property} - salary is ${obj[property]} dollars`);
+
+// console.log(min(0, -1));
+
+//task 2
+// const evenodd = (a) => {
+//     if(a % 2 == 0) {
+//         return "число четное";
+//     } else {
+//         return "число нечетное";
+//     }
+// };
+
+// console.log(evenodd(8));
+
+//task 3
+// function square(a) {
+//     console.log(Math.pow(a, 2));
 // }
 
-// 5 task
-let n = 1000,
-num = 0;
-while(n >= 50) {
-    n /= 2;
-    num++;
+// function sqr(b) {
+//     return Math.pow(b, 2);
+// }
+
+// square(3);
+// console.log(sqr(4));
+
+//task 4
+// let age = function() {
+//     let yearsOld = prompt('Сколько Вам лет?');
+//     yearsOld = Number(yearsOld);
+
+//     if(yearsOld < 0) {
+//         alert('Вы ввели неправильное значение');
+//     } else if(yearsOld >= 0 && yearsOld <= 12) {
+//         alert('Привет, друг!');
+//     } else if(yearsOld >= 13) {
+//         alert('Добро пожаловать!');
+//     } else {
+//         alert('smth get wrong');
+//     }
+// };
+
+// age();
+
+//task 5
+// const numbas = (first, second) => {
+//     if(isNaN(first) || isNaN(second)) {
+//         return "Одно или оба значения не являются числом";
+//     } else {
+//         return first * second;
+//     }
+// };
+
+// console.log(numbas('yyy', 6));
+
+//task 6
+// const cube = () => {
+//     let n = prompt("Введите число");
+
+//     if (isNaN(n)) {
+//         return "Переданный параметр не является числом";
+//     } else {
+//         n = Math.pow(n, 3);
+//         return `n в кубе равняется ${n}`;
+//     }
+// };
+
+// console.log(cube());
+
+//task 7
+// let circle1 = {
+//     radius: 5,
+//     getArea: function() {
+//         return Math.PI * Math.pow(this.radius, 2);
+//     },
+//     getPerimeter: function() {
+//         return 2 * Math.PI * this.radius;
+//     }
+// };
+
+// let circle2 = {
+//     radius: 6,
+//     getArea: function() {
+//         return Math.PI * Math.pow(this.radius, 2);
+//     },
+//     getPerimeter: function() {
+//         return 2 * Math.PI * this.radius;
+//     }
+// };
+
+// console.log(`Площадь первого круга равна ${circle1.getArea()}`);
+// console.log(`Площадь второго круга равна ${circle2.getArea()}`);
+// console.log(`Периметр первого круга равен ${circle1.getPerimeter()}`);
+// console.log(`Периметр второго круга равен ${circle2.getPerimeter()}`);
+
+//task 8
+// let btn = document.querySelectorAll("btn");
+// btn[1].addEventListener('click', function(event) {
+//     monthNum();
+// });
+
+function monthNum() {
+    let num = Number(prompt("Введите номер месяца"));
+    let season;
+
+    if(isNaN(num) || num <= 0 || num > 12) {
+        alert("Указанное значение не может обозначать номер месяца");
+        return "Указанное значение не может обозначать номер месяца";
+    } else {
+        switch(num) {
+            case 12:
+            case 1:
+            case 2:
+               season = 'Зима'; 
+               break;
+            case 3:
+            case 4:
+            case 5:
+                season = 'Весна'; 
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = 'Лето'; 
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = 'Осень'; 
+                break;
+            default:
+                season = 'Что-то здесь не так...';
+        }
+        alert(`Указанный номер месяца это ${season}`);
+        return season;
+    }
 }
-console.log(`final number is ${n} and loop has made ${num} iterations`);
 
-//6 task
-// let firstFriday = 3,
-// daysOfMonth = 31;
-// for(let i = 0; i <= daysOfMonth; i += 7) {
-//     console.log(`Сегодня пятница ${firstFriday}-е число. Необходимо подготовить отчет`);
-//     firstFriday += 7;
-// }
-
-
-
-});
+// });
